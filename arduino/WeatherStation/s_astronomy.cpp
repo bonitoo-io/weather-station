6c7ea8b2c806a560bd28d0e6d163600f255e2629
mirek.malecha@bonitoo.io <mirek.malecha@bonitoo.io>
2021-05-24 00:30:19 +0200

    Reduced memory consumption

--- arduino/WeatherStation/s_astronomy.cpp
+++ arduino/WeatherStation/s_astronomy.cpp
@@ -7,13 +7,19 @@
 
 extern OpenWeatherMapCurrentData currentWeather;
 
-SunMoonCalc::Result sunMoonData;
+time_t moonRise;
+time_t moonSet;
+uint8_t moonPhase;
 char moonAgeImage;
 
 void updateAstronomy(bool firstStart, const float lat, const float lon) {
   // 'now' has to be UTC, lat/lng in degrees not raadians
   SunMoonCalc smCalc = SunMoonCalc(time(nullptr), lat, lon);
-  sunMoonData = smCalc.calculateSunAndMoonData();
+  SunMoonCalc::Result sunMoonData = smCalc.calculateSunAndMoonData();
+  
+  moonRise = sunMoonData.moon.rise;
+  moonSet = sunMoonData.moon.set;
+  moonPhase = sunMoonData.moon.phase.index;
   moonAgeImage = (char) (65 + ((uint8_t) ((uint8_t)(26 * sunMoonData.moon.age / 30) % 26)));
 }
 
@@ -22,8 +28,8 @@ void drawAstronomy(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, i
   display->setFont(ArialMT_Plain_10);
   display->setTextAlignment(TEXT_ALIGN_LEFT);
   display->drawString(x + 38, y + 10, "Sun:" + strTime(currentWeather.sunrise, true) + "-" + strTime(currentWeather.sunset, true));
-  display->drawString(x + 38, y + 23, "Moon:" + strTime(sunMoonData.moon.rise, true) + "-" + strTime(sunMoonData.moon.set, true));
-  display->drawString(x + 38, y + 35, String( MOON_PHASES[sunMoonData.moon.phase.index]));
+  display->drawString(x + 38, y + 23, "Moon:" + strTime(moonRise, true) + "-" + strTime(moonSet, true));
+  display->drawString(x + 38, y + 35, String( MOON_PHASES[moonPhase]));
 
   display->setFont(MoonPhases_Regular_36);
   display->setTextAlignment(TEXT_ALIGN_LEFT);
