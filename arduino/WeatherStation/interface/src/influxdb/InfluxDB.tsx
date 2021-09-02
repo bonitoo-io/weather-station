6b10a411e875a167ec386f7c179c47cbb7d11fb5
vlastahajek <vlastimil.hajek@bonitoo.io>
2021-09-02 17:08:37 +0200

    chore: removing tab

--- arduino/WeatherStation/interface/src/influxdb/InfluxDB.tsx
+++ arduino/WeatherStation/interface/src/influxdb/InfluxDB.tsx
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import { Redirect, Switch, Route, RouteComponentProps } from 'react-router-dom'
 
-import { Tabs, Tab } from '@material-ui/core';
-
-
 import { MenuAppBar } from '../components';
 
 import InfluxDBSettingsController from './InfluxDBSettingsController';
@@ -18,10 +15,7 @@ class InfluxDB extends Component<SystemProps> {
 
   render() {
     return (
-      <MenuAppBar sectionTitle="InfluxDBSettings">
-        <Tabs value={this.props.match.url} onChange={this.handleTabChange} variant="fullWidth">
-          <Tab value="/influxdb/settings" label="InfluxDB Settings" />
-        </Tabs>
+      <MenuAppBar sectionTitle="InfluxDB Settings">
         <Switch>
           <Route exact path="/influxdb/settings" component={InfluxDBSettingsController} />
           <Redirect to="/influxdb/settings" />
