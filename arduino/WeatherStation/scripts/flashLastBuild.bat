13f43b58de0ec78cf1c757a5ba47a75631344873
vlastahajek <vlastimil.hajek@bonitoo.io>
2022-02-22 13:21:35 +0100

    chore: adding fw serial upload script

---
+++ arduino/WeatherStation/scripts/flashLastBuild.bat
@@ -0,0 +1,1 @@
+C:\Users\vlast\AppData\Local\Arduino15\packages\esp8266\tools\python3\3.7.2-post1/python3 -I C:\Users\vlast\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\3.0.2/tools/upload.py --chip esp8266 --port COM4 --baud 921600 --before default_reset --after hard_reset write_flash 0x0 c:\tmp\_ArduinoOutput\d1mini\weatherstation/WeatherStation.ino.bin 
