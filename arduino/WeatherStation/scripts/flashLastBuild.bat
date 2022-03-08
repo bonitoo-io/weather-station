81d236a90032e40bfc94e82510ca1d86612572f5
mirek.malecha@bonitoo.io <mirek.malecha@bonitoo.io>
2022-03-08 14:14:06 +0100

    feat: extracted parameters into variables

--- arduino/WeatherStation/scripts/flashLastBuild.bat
+++ arduino/WeatherStation/scripts/flashLastBuild.bat
@@ -1,1 +1,8 @@
-C:\Users\vlast\AppData\Local\Arduino15\packages\esp8266\tools\python3\3.7.2-post1/python3 -I C:\Users\vlast\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\3.0.2/tools/upload.py --chip esp8266 --port COM4 --baud 921600 --before default_reset --after hard_reset write_flash 0x0 c:\tmp\_ArduinoOutput\d1mini\weatherstation/WeatherStation.ino.bin 
+@echo off
+SET COM_PORT=COM6
+SET BIN_FILE=ws-firmware-0.58-rc9.bin
+SET SDK_VERSION=3.0.2
+SET SDK_VERSION_TOOLS=3.7.2-post1
+
+%APPDATA%\..\Local\Arduino15\packages\esp8266\tools\python3\%SDK_VERSION_TOOLS%\python3 -I %APPDATA%\..\Local\Arduino15\packages\esp8266\hardware\esp8266\%SDK_VERSION%\tools\upload.py --chip esp8266 --port %COM_PORT% --baud 921600 --before default_reset --after hard_reset write_flash 0x0 %BIN_FILE%
+@echo on
